45e448a69dd8a8a6ecaaec177fb4ca1eaeb7c6bf
Arthiat <arthurleywin912@gmail.com>
2025-04-25 21:09:06 +0400

    Delete day8 directory

--- day8/classwork/cw6.py
+++
@@ -1,6 +0,0 @@
-N = int(input("sheiyvanet rame ricxvi:"))
-
-if N>0:
-    print(int(N**0.5))
-if N<0:
-    print(False)
--- day8/homework/hw2.py
+++
@@ -1,7 +0,0 @@
-kodi = 1231
-shey_kod = int(input("sheiyvanet kodi: "))
-
-while shey_kod != kodi:
-    print("arasworia xelaxla cadet")
-    shey_kod = int(input("tavidan sheiyvanet kodi: "))
-print("tqven sworad sheiyvanet kodi")
